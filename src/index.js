512fba14a91d40b5f9930c0a895a2dfdf858a2f9
Jaime Barbosa <jaimebs@gmail.com>
2019-07-01 09:51:22 -0300

    feat: Created the navigation pages Feed and New. :sparkles:

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Routes from './routes';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Bem vindo Jaime!</Text>
-      </View>
-    );
-  }
+export default function App() {
+  return <Routes />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#9326c1'
-  },
-  welcome: {
-    fontSize: 28,
-    color: '#fdfdfd',
-    textAlign: 'center',
-    margin: 10
-  }
-});
--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity, Image, FlatList } from 'react-native';
+import api from '../services/api';
+
+import camera from '../assets/camera.png';
 
 export default class Feed extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    headerRight: (
+      <TouchableOpacity style={{ marginRight: 20 }} onPress={() => navigation.navigate('New')}>
+        <Image source={camera} />
+      </TouchableOpacity>
+    )
+  });
+
+  state = {
+    feeds: []
+  };
+
+  async componentDidMount() {
+    const posts = await api.get('posts');
+    this.setState({ feeds: posts.data });
+    //this.registerToSocket();
+    console.log(posts.data);
+  }
+
   render() {
     return (
       <View>
-        <Text> textInComponent </Text>
+        <FlatList
+          data={this.state.feeds}
+          keyExtractor={post => post._id}
+          renderItem={({ item }) => <Text>{item.author}</Text>}
+        />
       </View>
     );
   }
